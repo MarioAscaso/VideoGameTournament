3876e2c4a60c096d199e51486e931e0cf8fc2b58
Mario Ascaso <ascasovicentemario@gmail.com>
2025-12-10 11:02:07 +0100

    codigoo

--- src/main/java/com/daw/competitionGames/shared/domain/Game.java
+++ src/main/java/com/daw/competitionGames/shared/domain/Game.java
@@ -12,29 +12,34 @@ public class Game {
 
     private String name;
 
-    private int numMaxPlayers;
-
     private String description;
 
-    private Platform platform;
+    private AssociatedID associatedID;
+
+    private String bannerImage;
+
+    private String cardImage;
 
     public Game(){}
 
-    public Game(String name, int numMaxPlayers, String description, Platform platform) {
+    public Game(String name, String description, AssociatedID associatedID, String bannerImage, String cardImage) {
         this.name = name;
-        this.numMaxPlayers = numMaxPlayers;
         this.description = description;
-        this.platform = platform;
+        this.associatedID = associatedID;
+        this.bannerImage = bannerImage;
+        this.cardImage = cardImage;
     }
 
     public long getId() {return id;}
     public String getName() {return name;}
-    public int getNumMaxPlayers() {return numMaxPlayers;}
     public String getDescription() {return description;}
-    public Platform getPlatform() {return platform;}
+    public AssociatedID getAssociatedID() {return associatedID;}
+    public String getBannerImage() {return bannerImage;}
+    public String getCardImage() {return cardImage;}
 
     public void setName(String name) {this.name = name;}
-    public void setNumMaxPlayers(int numMaxPlayers) {this.numMaxPlayers = numMaxPlayers;}
     public void setDescription(String description) {this.description = description;}
-    public void setPlatform(Platform platform) {this.platform = platform;}
+    public void setAssociatedID(AssociatedID associatedID) {this.associatedID = associatedID;}
+    public void setBannerImage(String bannerImage) {this.bannerImage = bannerImage;}
+    public void setCardImage(String cardImage) {this.cardImage = cardImage;}
 }
