8004442070ec3a3ad820daaac804439a38a4bcd9
Mario Ascaso <ascasovicentemario@gmail.com>
2025-12-11 10:11:37 +0100

    codigo de ficheros

--- src/main/java/com/daw/competitionGames/gamescrud/deletegame/infrastructure/controller/DeleteGameController.java
+++ src/main/java/com/daw/competitionGames/gamescrud/deletegame/infrastructure/controller/DeleteGameController.java
@@ -1,9 +1,11 @@
 package com.daw.competitionGames.gamescrud.deletegame.infrastructure.controller;
 
 import com.daw.competitionGames.gamescrud.deletegame.application.DeleteGameApp;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
 public class DeleteGameController {
 
     private final DeleteGameApp deleteGameApp;
--- src/main/java/com/daw/competitionGames/shared/storagefiles/infrastructure/controller/FileController.java
+++ src/main/java/com/daw/competitionGames/shared/storagefiles/infrastructure/controller/FileController.java
@@ -1,4 +1,23 @@
 package com.daw.competitionGames.shared.storagefiles.infrastructure.controller;
 
+import com.daw.competitionGames.shared.storagefiles.domain.interfaces.StorageService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
 public class FileController {
+
+    private final StorageService storageService;
+
+    public FileController(StorageService storageService) {
+        this.storageService = storageService;
+    }
+
+    @PostMapping("/")
+    public String handleFileUpload(@RequestParam("file")MultipartFile aFile){
+        storageService.store(aFile);
+        return "redirect:/";
+    }
 }
